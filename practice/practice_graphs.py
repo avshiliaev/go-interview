e26f71ce274c06c3f6f33b3e3bd53d59426e6d33
Artem <a.v.shiliaev@gmail.com>
2021-06-06 17:27:27 +0200

    bst on matrix

--- practice/practice_graphs.py
+++ practice/practice_graphs.py
@@ -1,32 +1,50 @@
-def dfs(root, matrix):
+def dfs(root, adj_matrix):
     path = []
-    visited = [0 for _ in range(len(matrix))]
+    visited = [0 for _ in range(len(adj_matrix))]
 
-    def rec(n, m):
-        visited[n] = 1
-        path.append(n)  # Pre order
-        for j in range(len(m)):
-            if m[n][j] == 1 and not visited[j]:
-                rec(j, m)
-        # path.append(n)  # Post order
+    def rec(node, matrix):
+        visited[node] = 1
+        path.append(node)  # Pre order
+        for adj_node in range(len(matrix)):
+            if matrix[node][adj_node] == 1 and not visited[adj_node]:
+                rec(adj_node, matrix)
+        # path.append(node)  # Post order
 
-    rec(root, matrix)
+    rec(root, adj_matrix)
+    return path
+
+
+def bfs(root, matrix):
+    queue = [root]
+    path = []
+
+    while queue:
+        node = queue.pop(0)
+        path.append(node)
+        for i, v in enumerate(matrix[node]):
+            if v == 1:
+                queue.append(i)
     return path
 
 
 if __name__ == '__main__':
-
     """
-        0
-    1       2
-    3
+              0
+        1           2
+    3       4   5       6
     
     """
 
     adj = [
-        [0, 1, 1, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0],  # 0
+
+        [0, 0, 0, 1, 1, 0, 0],  # 1
+        [0, 0, 0, 0, 0, 1, 1],  # 2
+
+        [0, 0, 0, 0, 0, 0, 0],  # 3
+        [0, 0, 0, 0, 0, 0, 0],  # 4
+        [0, 0, 0, 0, 0, 0, 0],  # 5
+        [0, 0, 0, 0, 0, 0, 0],  # 6
     ]
     print(dfs(0, adj))
+    print(bfs(0, adj))
