50594bae91327b14aed45972fb395015ba3d251f
Artem <a.v.shiliaev@gmail.com>
2021-06-06 20:40:33 +0200

    complete and exact topological sort

--- practice/top_sort.py
+++ practice/top_sort.py
@@ -1,31 +1,29 @@
-def iterative_topological_sort(graph_, root):
-    """
-    Two stacks because we have two DFSs:
-        see why here https://www.geeksforgeeks.org/topological-sorting/
-        and here https://stackoverflow.com/a/47234034/10202443
+def top_sort(graph_):
+    # https://stackoverflow.com/a/62758232/10202443
+    # https://sharmaeklavya2.github.io/theoremdep/nodes/graph-theory/dfs/toposort.html
+    visited = {}
+    result = []
 
-        in fact this is correct: https://stackoverflow.com/a/67291884/10202443
-        instead of choosing the root arbitrary, we want to transpose the graph first and then get a node which has
-        NO incoming edges.
+    # 1. Transpose
+    transposed_graph_ = {k: [] for k, v in graph_.items()}
+    for k, v in graph_.items():
+        for val in v:
+            transposed_graph_[val].append(k)
 
-    :param graph_:
-    :param root:
-    :return:
-    """
-    seen = set()
-    order = []
-    stack_one = [root]
-    stack_two = []
-    while stack_one:
-        node = stack_one.pop(-1)
-        if node not in seen:
-            seen.add(node)
-            stack_one += graph_[node]
-            while stack_two and node not in graph_[stack_two[-1]]:
-                order.insert(0, stack_two.pop(-1))
-            stack_two.append(node)
+    # 2. Define dfs
+    def dfs(node):
+        if visited.get(node, False):
+            return
+        visited[node] = True
+        for adj in transposed_graph_[node]:
+            dfs(adj)
+        result.append(node)
 
-    return stack_two + order
+    # 3. Dfs through each node
+    for i in transposed_graph_.keys():
+        dfs(i)
+
+    return result
 
 
 if __name__ == '__main__':
@@ -34,13 +32,26 @@ if __name__ == '__main__':
      \             /
       -->-- c -->--
     """
-    graph = {
+    # why the graph has to be transposed: https://stackoverflow.com/a/67291884/10202443
+    graph_one = {
         'a': ['b', 'c'],
         'b': ['d'],
         'c': ['d'],
         'd': ['e'],
-        'e': []
+        'e': [],
     }
     # [a, b, c, d, e]
 
-    print(iterative_topological_sort(graph, 'a'))
+    # https://www.geeksforgeeks.org/python-program-for-topological-sorting/#:~:text=Topological%20sorting%20for%20Directed%20Acyclic,4%202%203%201%200%E2%80%9D.
+    graph_two = {
+        5: [0, 2],
+        4: [0, 1],
+        3: [1],
+        2: [3],
+        1: [],
+        0: []
+    }
+    # 5 4 2 3 1 0
+
+    print(top_sort(graph_one))
+    print(top_sort(graph_two))
